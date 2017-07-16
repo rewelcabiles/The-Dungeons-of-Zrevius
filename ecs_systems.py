5a9ba7b5d268c283d3bc23bbb410e137b8ea9c68
Rewel Cabiles <rewel.sicuan@gmail.com>
2017-07-16 19:27:56 +0800

    Cleaned up code, removed some print statements

--- ecs_systems.py
+++ ecs_systems.py
@@ -1,14 +1,5 @@
-import ecs_components
 import random
 import json
-
-class COMPS:
-    NONE = 0
-    POS  = 1L << 0
-    INV  = 2L << 1
-    DESC = 1L << 2
-    ROOM = 1L << 3
-
 
 class World():
     def __init__(self):
@@ -34,6 +25,7 @@ class World():
         }
         self.entity_id_max = 1000
 
+
     def assign_entity_id(self):
         while True:
             entity_id = random.randint(1, self.entity_id_max)
@@ -41,17 +33,21 @@ class World():
                 self.WORLD['mask'][entity_id] = self.COMPS['none']
                 return entity_id
 
+
     def destroy_entity(entity_id):
         self.all_entities.remove(entity_id)
 
-#Make ecs_components.py a json file instead.
+
+
 class Factory():
+
     def __init__(self,WORLD):
         self.world = WORLD
         with open('archetype.json') as data_file:
             self.archetypes = json.load(data_file)
         with open('components.json') as component_files:
             self.components = json.load(component_files)
+
 
     def create_components(self, comp, ent_id):
         WORLD = self.world.WORLD
@@ -60,7 +56,6 @@ class Factory():
             WORLD['descriptor'][ent_id] = self.components['descriptor'].copy()
             WORLD['mask'][ent_id] |= self.world.COMPS['descriptor']
         elif comp == "position":
-            #WORLD['position'][ent_id]  = ecs_components.position()
             WORLD['position'][ent_id]  = self.components['position'].copy()
             WORLD['mask'][ent_id] |= self.world.COMPS['position']
         elif comp == 'isroom':
@@ -69,7 +64,6 @@ class Factory():
         elif comp == 'inventory':
             WORLD['inventory'][ent_id] = self.components['inventory'].copy()
             WORLD['mask'][ent_id] |= self.world.COMPS['inventory']
-
         else:
             print "ERROR: No Known Component Name!"
 
@@ -79,17 +73,13 @@ class Factory():
         ent_id = self.world.assign_entity_id()
         for component in self.archetypes['room'].keys():
             self.create_components(component, ent_id)
-            print component
         self.world.WORLD['position'][ent_id]['x'] = x
         self.world.WORLD['position'][ent_id]['y'] = y
-        print ent_id
-        #print self.world.WORLD['position'][ent_id]['x'], self.world.WORLD['position'][ent_id]['y']
         return ent_id
 
 
 #w = World()
 #f = Factory(w)
-#f.room_creator()
 
 
 
