7af9d56fccedef7dba55035b0406a03ce70c0c7c
Rewel Cabiles <rewel.sicuan@gmail.com>
2017-07-19 18:31:51 +0800

    Emptied out the main.py file

--- main.py
+++ main.py
@@ -1,48 +1,9 @@
 import os
 import json
 
-game = True
-
-save_loc = "save_01"
-has_save = False
-
-def startup():
-    if os.path.ispath(save_loc):
-        has_save = True
-
-def newgame():
-    pass
-
-def PROMPT(text, answer, callback):
-    print(text)
-    callback(raw_input("> "))
-
-def main_menu():
-    if has_save == True:
-        while True:
-            print("1) New Game")
-            print("2) Continue")
-            user = raw_input(">> ");
-            if user == "1":
-                new_game()
-            elif user == "2":
-                continue_game()
-            else:
-                print("Invalid Command." )
-
-def continue_game():
-    pass
-
-def new_game():
-    Character = {
-            name : None,
-            place: [],
-    }
-    GAME(Character)
-
-
-def GAME(savefile):
-    pass
+# This is where the game will run from.
+# Still empty, while i work on the back end a little more
+# beep boop
 
 
 
