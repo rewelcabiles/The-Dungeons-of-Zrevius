ba6315c716991c38623aee45a8cf721f1cb53028
Rewel Cabiles <rewel.sicuan@gmail.com>
2018-01-03 18:14:42 +0800

    Added a few comments

--- bin/ecs_systems.py
+++ bin/ecs_systems.py
@@ -2,41 +2,8 @@ import random
 import json
 import copy
 
-class World():
-
-    def __init__(self):
-
-        #Loads the files that contains all components
-        with open ('components.json') as component_files:
-           components = json.load(component_files)
-
-        # This is where all the specific component dicts reside, inside the WORLD dictionary
-        self.WORLD = {}
-        self.COMPS = {}
-        # Creates the dictionaries for each component in the components.json files
-        self.COMPS['none'] = 1 << 0
-        iterator = 1
-        for key in components:
-            self.WORLD[key] = {}
-            self.COMPS[key] = 1 << iterator
-            iterator += 1
-        self.entity_id_max = 1000
-        self.factory = Factory(self)
-
-
-    def assign_entity_id(self):
-        while True:
-            entity_id = random.randint(1, self.entity_id_max)
-            if entity_id not in list(self.WORLD['mask'].keys()):
-                self.WORLD['mask'][entity_id] = self.COMPS['none']
-                return entity_id
-
-
-
-    def destroy_entity(entity_id):
-        self.all_entities.remove(entity_id) # This hasn't been updated to the new system yet.
-
-
+# Bridges the gap between the information in the json files, and the dungeon creator.
+# Loads json files, and uses the data to create objects for the dungeon.
 
 class Factory():
 
@@ -143,15 +110,5 @@ class Factory():
         self.WORLD['weapon'][ent_id] =  self.archetypes[weapon_type]['weapon'].copy()
         self.lorify(ent_id)
         return ent_id
-
-#w = World()
-#f = Factory(w)
-
-
-
-
-
-
-
 
 
