8f4f6b5b498982ee6f10f1f66629163b25107cec
Rewel Cabiles <rewel.sicuan@gmail.com>
2018-01-03 20:29:52 +0800

    Removed old code

--- bin/game_functions.py
+++ bin/game_functions.py
@@ -7,10 +7,12 @@ import json
 # 
 # 
 
+
 class Interface:
 
 	def __init__(self):
 		self.init_world()
+
 
 	def init_world(self):
 		dg = dun_gen()
@@ -23,15 +25,4 @@ class Interface:
 	def save_dungeon(self):
 		with open('data/data.json', 'w') as save_file:
 			json.dump(self.world.WORLD, save_file, indent=4, sort_keys=True)
-
-	def move_ent(self, eid): # eid will refer to the entity ID of entities.
-		pass
-
-
-	def get_ent_pos(self, eid):
-		pass
-
-
-	def get_ent_stats(self, eid):
-		pass
 
